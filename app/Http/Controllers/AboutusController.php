7b01307283ca7325fed2cb1af235a8f08aa79325
ms-hamid <hamid.albase@gmail.com>
2025-07-03 18:52:01 +0700

    feat: Update About Us page to display team members and principles, and fix password view extension

--- app/Http/Controllers/AboutusController.php
+++ app/Http/Controllers/AboutusController.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\StoreTeamRequest;
+use App\Http\Requests\UpdateTeamRequest;
+use App\Models\OurPrinciple;
+use App\Models\OurTeam;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
 class AboutusController extends Controller
 {
     public function aboutus(){
-        return view('pages.aboutus');
+        $teams = OurTeam::all()->take(6);
+        $sprinciples = OurPrinciple::take(4)->get();
+        return view('pages.aboutus', compact('teams', 'sprinciples'));
     }
+
 }
